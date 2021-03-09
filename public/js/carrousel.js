1ffd29089d2d3e588bfb19469790999135ffac15
Tomtom-dev <thomasheddadji0@gmail.com>
2021-03-09 21:21:22 +0100

    make the carousel responsive

--- public/js/carrousel.js
+++ public/js/carrousel.js
@@ -1,7 +1,9 @@
 const carouselImage = document.querySelector('.caroussel__images')
 const carouselButtons = document.querySelectorAll(".caroussel__button")
 const numberOfImages = document.querySelectorAll(".caroussel__images img").length;
+const pictureOnly = document.querySelector('.caroussel__images img')
 
+console.log(pictureOnly);
 let imageIndex=1;
 let tranlateX=0;
 
@@ -10,14 +12,16 @@ carouselButtons.forEach(button =>{
         if(event.target.id === "previous"){
             if(imageIndex !== 1){
                 imageIndex --;
-                tranlateX +=55;
+                tranlateX += pictureOnly.offsetWidth;
+                console.log(pictureOnly.offsetWidth);
             }
         }else{
             if (imageIndex !== numberOfImages){
                 imageIndex++;
-                tranlateX -= 55;
+                tranlateX -= pictureOnly.offsetWidth;
+                console.log(pictureOnly.offsetWidth);
             }
         }
-        carouselImage.style.transform = `translateX(${tranlateX}rem)`
+        carouselImage.style.transform = `translateX(${tranlateX}px)`
     })
 })
